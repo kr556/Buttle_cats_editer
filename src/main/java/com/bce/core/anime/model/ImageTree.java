4e8f36af7007b931cf9867f6d5b1a25b3c39ff9e
bondrewd <nanachi7711@outlook.jp>
2023-07-11 05:02:20 +0900

    Changes

--- src/main/java/com/bce/core/anime/model/ImageTree.java
+++ src/main/java/com/bce/core/anime/model/ImageTree.java
@@ -1,22 +1,23 @@
 package com.bce.core.anime.model;
 
 import com.bce.core.anime.model.image.AbsImage;
+import com.bce.core.anime.model.image.FolderImage;
 import com.bce.core.anime.model.image.TreeEndImage;
 
 import java.awt.image.BufferedImage;
 
 public class ImageTree extends AbsModelTree<AbsImage> {
 
-    public ImageTree(AbsImage image, AbsModelTree<AbsImage> parent) {
-        super(parent.size(), parent.getId(), () -> image);
+    public ImageTree(ModelStream parent, AbsImage image) {
+        super(parent, parent.get().tree_top, image);
     }
 
-    public ImageTree(BufferedImage bf, AbsModelTree<AbsImage> parent) {
-        super(parent.size(), parent.getId(), () -> new TreeEndImage(bf));
+    public ImageTree(ModelStream  parent, BufferedImage image) {
+        super(parent, parent.get().tree_top, new FolderImage(image));
     }
 
     @Override
     public AbsImage getImage() {
-        return image.get();
+        return image;
     }
 }
